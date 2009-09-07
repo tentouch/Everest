a1ed6470e1b0f4843dbdafee07aae9aef57a0181
tentdeveloper <tentouch.developer@gmail.com>
2009-09-07 18:06:36 +0200

    Dobavihme super ideya

---
+++ Classes/superidea.cpp
@@ -0,0 +1,11 @@
+/*
+ *  superidea.cpp
+ *  CoolCam
+ *
+ *  Created by Тен Тач on 9/7/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "superidea.h"
+
---
+++ Classes/superidea.h
@@ -0,0 +1,9 @@
+/*
+ *  superidea.h
+ *  CoolCam
+ *
+ *  Created by Тен Тач on 9/7/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
